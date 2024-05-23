027be207c221b3c47e93c508efb5653843ca0e5b
danielac32 <danielquinteroac32@gmail.com>
2024-05-22 23:18:28 -0400

    first

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { RepresentanteModule } from './registro/representante/representante.module';
-import { EstudianteEstatusModule } from './registro/estudiante-estatus/estudiante-estatus.module';
 import { RecordAcademicoModule } from './registro/record-academico/record-academico.module';
 import { PerfilEstudianteModule } from './registro/perfil-estudiante/perfil-estudiante.module';
 import { EstudianteModule } from './registro/estudiante/estudiante.module';
 
 
 @Module({
-  imports: [ AuthModule,ConfigModule.forRoot(), RepresentanteModule, EstudianteEstatusModule, RecordAcademicoModule, PerfilEstudianteModule, EstudianteModule],
+  imports: [ AuthModule,ConfigModule.forRoot(), RepresentanteModule, RecordAcademicoModule, PerfilEstudianteModule, EstudianteModule],
 })
 export class AppModule {}
--- src/registro/estudiante-estatus/dto/update-estudiante-estatus.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateEstudianteEstatusDto } from './create-estudiante-estatus.dto';
-
-export class UpdateEstudianteEstatusDto extends PartialType(CreateEstudianteEstatusDto) {}
